a2503dec01379a05514629e3521c402e2a5eb9f2
Neto-IBS <140271677+Neto616@users.noreply.github.com>
2025-04-08 11:36:27 -0600

    First commit

---
+++ app.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require("path")
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+
+app.use(require(path.join(__dirname, "./rutas.js")))
+
+// app.post('/servidorpost', async (req, res) => {
+//     try {
+//         const sql = 'INSERT INTO sensor (dato, segundo) VALUES (?, ?)';
+//         const values = [req.body.dato, req.body.segundo];
+
+//         const [result] = await db.query(sql, values);
+
+//         res.json({ message: 'Datos insertados con éxito', id: result.insertId });
+//     } catch (err) {
+//         console.error('Error al insertar datos:', err);
+//         res.status(500).json({ error: 'Error al insertar datos en la base de datos' });
+//     }
+// });
+
+app.listen(port, () => {
+  console.log(`Servidor escuchando en http://localhost:${port}`);
+});
---
+++ rutas.js
@@ -0,0 +1,127 @@
+const express = require("express");
+const dataBase = require("mysql2");
+const route = express.Router();
+
+let data = {
+    flag: false,
+    id: 0
+};
+
+const dbConfig = {
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASS,
+    database: process.env.DB
+};
+
+const db = dataBase.createPool(dbConfig).promise();
+
+route.get('/servidorget', async (req, res) => {
+    try {
+      const [results] = await db.query('SELECT * FROM fuga_gas');
+    console.log("El resultado es: ", results)
+      res.json(results);
+    } catch (err) {
+      res.status(500).send(err.message);
+    }
+});
+
+/**
+ * En esta ruta debe entrar unicamente las particulas por millon en el cuerpo d la solicitud
+ */
+
+route.post('/fuga_gas', async (req, res) => {
+    try {
+        console.log(req.body)
+        
+        if (!data["flag"]) {
+            data["flag"] = true;
+            
+            await db.query(
+                `insert into fuga_gas
+                (tiempo_inicial)
+                values
+                (now());`);
+    
+            const [rows] = await db.query("select LAST_INSERT_ID() as id;");
+
+            data["id"] = rows[0].id;
+        }
+
+        await db.query(
+            `insert into detalles_fuga
+             (id_fuga, tiempo, ppm)
+             values
+             (?, now(), ?)`
+            ,[data["id"], ...Object.values(req.body)]);
+
+        console.log(data);
+
+        return res.json({
+            estatus: 1, 
+            info: {
+                message: "Se guardaron los datos de la fuga",
+                data
+            }
+        });
+    } catch (error) {
+        data = {
+            flag: false,
+            id: 0
+        };
+
+        console.error(error);
+        return res.json({ 
+            estatus: 0, 
+            info: {
+                message: "Ha ocurrido un error: "+error
+            }
+        })
+    }
+})
+
+
+route.put('/fin_fuga', async (req, res) => {
+    try {
+        if(!data["flag"]){
+            return res.json({ 
+                estatus: 2, 
+                info: {
+                    message: "No se puede finalizar una fuga si no fue iniciada"
+                }
+            })
+        }
+
+        await db.query(
+            `update fuga_gas
+            set tiempo_final = now()
+            where id = ?`,
+        [data["id"]]);
+
+        console.log(data);
+        data = {
+            flag: false,
+            id: 0
+        }
+        return res.json({
+            estatus: 1,
+            info: {
+                message: "Se ha finalizado la fuga"
+            }
+        })
+    } catch (error) {
+        data = {
+            flag: false,
+            id: 0
+        }
+        console.log(error);
+        return res.json({ 
+            estatus: 0,
+            info: {
+                message: "Ha ocurrido un error: "+error
+            } 
+        });
+    }
+})
+
+module.exports = route;
