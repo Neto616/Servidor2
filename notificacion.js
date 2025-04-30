6d6260cfef50f4f99728ebf34872e7d99d588e99
Neto-IBS <140271677+Neto616@users.noreply.github.com>
2025-04-29 18:45:53 -0600

    Uso de promesas para las notificaciones

--- notificacion.js
+++ notificacion.js
@@ -1,53 +1,17 @@
-const admin = require('firebase-admin');
-
-let initialized = false;
-
-function initFirebase() {
-    if (!initialized) {
-        const serviceAccount = {
-            type: process.env.type,
-            project_id: process.env.project_id,
-            private_key: process.env.private_key.replace(/\\n/g, '\n'),
-            client_email: process.env.client_email,
-            client_id: process.env.client_id,
-            auth_uri: process.env.auth_uri,
-            token_uri: process.env.token_uri,
-            auth_provider_x509_cert_url: process.env.auth_provider_x509_cert_url,
-        };
-
-        admin.initializeApp({
-            credential: admin.credential.cert(serviceAccount),
-        });
-
-        initialized = true;
-    }
-}
-
 async function notification(devices, title, body, extra_information) {
-    initFirebase(); // inicializa solo una vez
+    initFirebase();
 
-    let message = {
-        notification: {
-            title,
-            body
-        },
-        data: {
-            extra_information: extra_information
-        },
-        token: ""
-    };
+    const messages = devices.map(token => ({
+        notification: { title, body },
+        data: { extra_information },
+        token: token
+    }));
 
     try {
-        devices.forEach(async (element) => {
-            message["token"] = element
-            await admin.messaging().send(message);
-        });
-
-        return ;
+        const responses = await Promise.all(messages.map(msg => admin.messaging().send(msg)));
+        return responses; // Devuelve un array de respuestas
     } catch (error) {
         console.error("❌ Error al enviar la notificación:", error);
         throw error;
     }
 }
-
-module.exports = notification;
