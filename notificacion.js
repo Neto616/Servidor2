bc22737e5f8f203f29a7d8c4ee754e319160ac7c
Neto-IBS <140271677+Neto616@users.noreply.github.com>
2025-04-29 18:47:07 -0600

    Uso de promesas para las notificaciones

--- notificacion.js
+++ notificacion.js
@@ -1,3 +1,28 @@
+const admin = require('firebase-admin');
+
+let initialized = false;
+
+function initFirebase() {
+    if (!initialized) {
+        const serviceAccount = {
+            type: process.env.type,
+            project_id: process.env.project_id,
+            private_key: process.env.private_key.replace(/\\n/g, '\n'),
+            client_email: process.env.client_email,
+            client_id: process.env.client_id,
+            auth_uri: process.env.auth_uri,
+            token_uri: process.env.token_uri,
+            auth_provider_x509_cert_url: process.env.auth_provider_x509_cert_url,
+        };
+
+        admin.initializeApp({
+            credential: admin.credential.cert(serviceAccount),
+        });
+
+        initialized = true;
+    }
+}
+
 async function notification(devices, title, body, extra_information) {
     initFirebase();
 
@@ -15,3 +40,5 @@ async function notification(devices, title, body, extra_information) {
         throw error;
     }
 }
+
+module.exports = notification;
